de8192826503feabf512bd28fae97199100e91a4
Josh-Muleshi <jmuleshi2@gmail.com>
2022-07-17 16:54:36 +0200

    main navigation and restructure folder logic

---
+++ app/src/main/java/com/example/notes/feature_node/data/data_source/NoteDao.kt
@@ -0,0 +1,4 @@
+package com.example.notes.feature_node.data.data_source
+
+interface NoteDao {
+}
---
+++ app/src/main/java/com/example/notes/feature_node/presentation/navigation/NavHost.kt
@@ -0,0 +1,41 @@
+package com.example.notes.feature_node.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+
+@Composable
+fun MainNavHost(navController: NavHostController){
+    NavHost(navController = navController, startDestination =  Screen.HomeScreen.route) {
+        composable(route = Screen.HomeScreen.route) {
+            //HomeScreen(navController = navController)
+        }
+        composable(
+            route = Screen.AddEditNoteScreen.route +
+                    "?noteId={noteId}&noteColor={noteColor}",
+            arguments = listOf(
+                navArgument(
+                    name = "noteId"
+                ) {
+                    type = NavType.IntType
+                    defaultValue = -1
+                },
+                navArgument(
+                    name = "noteColor"
+                ) {
+                    type = NavType.IntType
+                    defaultValue = -1
+                },
+            )
+        ) {
+            val color = it.arguments?.getInt("noteColor") ?: -1
+//            AddEditNoteScreen(
+//                navController = navController,
+//                noteColor = color
+//            )
+        }
+    }
+}
